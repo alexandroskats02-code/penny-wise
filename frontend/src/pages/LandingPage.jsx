c9ccf4ac2ef4a9362c324bcce6ceb07efd0a0f8e
Alexandros Kats <alexandroskats02@gmail.com>
2026-09-04 03:43:35 +0300

    Create landing page structure

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      {/*This is where NavBar is going to be
+      with the Header and the NavLinks for Sign-up and Log-in*/}
+      <main>
+        <section>
+          <h1>Title</h1>
+          <p>A description of how this works</p>
+        </section>
+
+        <section>
+          <h2>Benefits of the app</h2>
+          
+        </section>
+
+        <section>
+          <h2>Get Started Today</h2>
+          <button>Sign Up</button>
+        </section>
+      </main>
+
+      {/*This is where the Footer is going to be*/}
+    </>
+  );
+};
+
+export default LandingPage;
